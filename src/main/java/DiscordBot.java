b9a55f32c39cb2b2403ec93a579321d0b9f1849b
Vivek George <vivek.george1998@gmail.com>
2016-11-12 11:08:50 -0500

    Update DiscordBot.java

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -18,60 +18,30 @@ public class DiscordBot {
     private static LocalDateTime time = LocalDateTime.now();
 
     public static void main(String[] args) throws DiscordException {
-        characters.put("4277","Oskar");
-        characters.put("2966","Astol");
-        characters.put("5510","Grald");
-        characters.put("0810","Fosco");
-        characters.put("0484","Khiron");
-        characters.put("5473","Mightus");
-        characters.put("7284","Strymash");
-        characters.put("2866","Kriv");
-        characters.put("4343","DM");
-        characters.put("5645","Sunshine");
+        String name[][] = new String[10][2] =
+        {
+            ["4277","Oskar"],         
+            ["2966","Astol"],
+            ["5510","Grald"],
+            ["0810","Fosco"],
+            ["0484","Khiron"],
+            ["5473","Mightus"],
+            ["7284","Strymash"],
+            ["2866","Kriv"],
+            ["4343","DM"],
+            ["5645","Sunshine"]
+        }
 
-        dailies.put("4277",LocalDateTime.now().minusDays(1));
-        dailies.put("2966",LocalDateTime.now().minusDays(1));
-        dailies.put("5510",LocalDateTime.now().minusDays(1));
-        dailies.put("0810",LocalDateTime.now().minusDays(1));
-        dailies.put("0484",LocalDateTime.now().minusDays(1));
-        dailies.put("5473",LocalDateTime.now().minusDays(1));
-        dailies.put("7284",LocalDateTime.now().minusDays(1));
-        dailies.put("2866",LocalDateTime.now().minusDays(1));
-        dailies.put("4343",LocalDateTime.now().minusDays(1));
-        dailies.put("5645",LocalDateTime.now().minusDays(1));
-
-        credits.put("4277",0);
-        credits.put("2966",0);
-        credits.put("5510",0);
-        credits.put("0810",0);
-        credits.put("0484",0);
-        credits.put("5473",0);
-        credits.put("7284",0);
-        credits.put("2866",0);
-        credits.put("4343",0);
-        credits.put("5645",0);
-
-        crits.put("4277",0);
-        crits.put("2966",0);
-        crits.put("5510",0);
-        crits.put("0810",0);
-        crits.put("0484",0);
-        crits.put("5473",0);
-        crits.put("7284",0);
-        crits.put("2866",0);
-        crits.put("4343",0);
-        crits.put("5645",0);
-
-        critFails.put("4277",0);
-        critFails.put("2966",0);
-        critFails.put("5510",0);
-        critFails.put("0810",0);
-        critFails.put("0484",0);
-        critFails.put("5473",0);
-        critFails.put("7284",0);
-        critFails.put("2866",0);
-        critFails.put("4343",0);
-        critFails.put("5645",0);
+        for (int i = 0; i < name.length; i++)
+        {
+            characters.put(name[i][0],name[i][0]);   
+            dailies.put(name[i][0],LocalDateTime.now().minusDays(1));
+            credits.put(name[i][0], 0);
+            crits.put(name[i][0],0);
+            critFails.put(name[i][0],0);
+            
+        }
+        
         client = new ClientBuilder().withToken(/*BOT TOKEN*/).login();
         client.getDispatcher().registerListener(new AnnotationListener(characters,dailies,credits,crits,critFails));
 
